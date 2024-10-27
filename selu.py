8dfbd06ac43dc634c66e8d877b7d2c08e1555f9c
Arjun Sarao <arjunsarao727@gmail.com>
2024-10-27 13:32:50 -0400

    changing relu to selu as described in the paper.

---
+++ selu.py
@@ -0,0 +1,24 @@
+import math
+
+from tinygrad.tinygrad.engine.lazy import LazyBuffer
+from tinygrad.tinygrad.tensor import Function
+
+
+# https://github.com/pytorch/pytorch/blob/96aaa311c0251d24decb9dc5da4957b7c590af6f/torch/nn/modules/activation.py#L507
+class Selu(Function):
+    _alpha: float = 1.6732632423543772848170429916717
+    _lambda: float = 1.0507009873554804934193349852946
+
+    def forward(self, x: LazyBuffer) -> LazyBuffer:
+        self.ret = self._lambda * LazyBuffer.where(
+            x >= 0, x, self._alpha * ((x * (1 / math.log(2))).exp2() - 1)
+        )
+        return self.ret
+
+    def backward(self, grad_output: LazyBuffer) -> LazyBuffer:
+        dx = LazyBuffer.where(
+            self.ret >= 0,
+            self._lambda,
+            self._lambda * self._alpha * (self.ret * (1 / math.log(2))).exp2(),
+        )
+        return dx * grad_output
